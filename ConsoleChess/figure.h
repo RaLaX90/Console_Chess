5cce7ba6c33cbd60a0203cc1ee2025a5d907d2ee
RaLaX90 <cool.suhomlin@mail.ru>
2020-04-12 16:14:31 +0300

    Add new function to "OOP-style". New logic to "NO OOP-style".

--- ConsoleChess/figure.h
+++ ConsoleChess/figure.h
@@ -5,44 +5,55 @@ using namespace std;
 
 struct figure
 {
-private:
-	string icon;
 public:
-	figure(string icon);
+	enum class colors { WHITE, BLACK };
+
+	figure(string icon, colors color, int x, int y);
 
 	string getIcon();
 
+private:
+	string icon;
+	colors color;
+
+	int x;
+	int y;
 };
 
 
-figure::figure(string icon) {
+figure::figure(string icon, colors color, int x, int y) {
 	this->icon = icon;
+	this->color = color;
+	this->x = x;
+	this->y = y;
 }
 
 string figure::getIcon() {
 	return icon;
 }
 
+
+
 struct rook : public figure {
-	rook(string icon) : figure(icon) {}
+	rook(string icon, colors color, int x, int y) : figure(icon, color, x, y) {}
 };
 
 struct knight : public figure {
-	knight(string icon) : figure(icon) {}
+	knight(string icon, colors color, int x, int y) : figure(icon, color, x, y) {}
 };
 
 struct bishop : public figure {
-	bishop(string icon) : figure(icon) {}
+	bishop(string icon, colors color, int x, int y) : figure(icon, color, x, y) {}
 };
 
 struct queen : public figure {
-	queen(string icon) : figure(icon) {}
+	queen(string icon, colors color, int x, int y) : figure(icon, color, x, y) {}
 };
 
 struct king : public figure {
-	king(string icon) : figure(icon) {}
+	king(string icon, colors color, int x, int y) : figure(icon, color, x, y) {}
 };
 
 struct pawn : public figure {
-	pawn(string icon) : figure(icon) {}
+	pawn(string icon, colors color, int x, int y) : figure(icon, color, x, y) {}
 };
